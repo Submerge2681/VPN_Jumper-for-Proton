e418e8a8305a5267b04c36703d70c8e8b5d8084f
echoscar <115612009+echoscar@users.noreply.github.com>
2023-02-20 15:51:24 +0530

    Update vpn_jumper.py

--- vpn_jumper.py
+++ vpn_jumper.py
@@ -4,16 +4,19 @@ import os
 from time import sleep
 import random
 
-codeList = ["TR", "US", "DE", "NL", "CH", "CA-W", "GB"]
-
+codeList = ["CH", "IS", "SE", "RO", "DE", "ES", "AT", "GR"] #Switzerland(CH), Iceland(IS), Sweden(SE), Romania(RO), Germany(DE), Spain(ES), Austria(AT), Greece(GR)
+#Germany, Sweden and spain are part of the 14 eyes.
 try:
-  os.system("windscribe connect")
+  os.system("protonvpn-cli killswitch --on")
+  os.system("protonvpn-cli connect -f") ###Connects to the fastest server!
+  os.system("protonvpn-cli status")
   while True:
     codeChoice = random.choice(codeList)
-    sleep(random.randrange(12,30))
+    sleep(random.randrange(600,900))
     print("#Changing the IP Address...")
-    os.system("windscribe connect " + codeChoice)
-
+    os.system("protonvpn-cli connect " + "--cc " + codeChoice)  #Concatinate Proton commands with country code(--cc) from the codelist!
+    os.system("protonvpn-cli status")
 except:
-  os.system("windscribe disconnect")
+  os.system("protonvpn-cli killswitch --off")
+  os.system("protonvpn-cli disconnect")
   print("Error occured")
