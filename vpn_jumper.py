fec91b94ebfdd40408c45b1847ff3f83e5d88c92
echoscar <115612009+echoscar@users.noreply.github.com>
2023-02-20 16:36:16 +0530

    Update vpn_jumper.py

--- vpn_jumper.py
+++ vpn_jumper.py
@@ -7,7 +7,6 @@ import random
 codeList = ["CH", "IS", "SE", "RO", "DE", "ES", "AT", "GR"] #Switzerland(CH), Iceland(IS), Sweden(SE), Romania(RO), Germany(DE), Spain(ES), Austria(AT), Greece(GR)
 #Germany, Sweden and spain are part of the 14 eyes.
 try:
-  os.system("protonvpn-cli killswitch --on")
   os.system("protonvpn-cli connect -f") ###Connects to the fastest server!
   os.system("protonvpn-cli status")
   while True:
@@ -17,6 +16,5 @@ try:
     os.system("protonvpn-cli connect " + "--cc " + codeChoice)  #Concatinate Proton commands with country code(--cc) from the codelist!
     os.system("protonvpn-cli status")
 except:
-  os.system("protonvpn-cli killswitch --off")
   os.system("protonvpn-cli disconnect")
   print("Error occured")
