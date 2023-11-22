62abbe1ff4aab5c7fafeeb69714ca59de32a5cb3
Submerge2681 <115612009+Submerge2681@users.noreply.github.com>
2023-11-22 08:00:15 +0530

    Update vpn_jumper.py

--- vpn_jumper.py
+++ vpn_jumper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import os
 from time import sleep
 import random
@@ -7,13 +6,25 @@ import random
 codeList = ["CH", "IS", "SE", "RO", "DE", "ES", "AT", "GR"] #Switzerland(CH), Iceland(IS), Sweden(SE), Romania(RO), Germany(DE), Spain(ES), Austria(AT), Greece(GR)
 #Germany, Sweden and spain are part of the 14 eyes.
 
+previous = ''
+
+def lottery():
+  global previous
+  codeChoice = random.choice(codeList)
+  if codeChoice == previous:
+    while codeChoice == previous:
+      codeChoice = random.choice(codeList)
+  previous = codeChoice
+  print("\n#Changing the IP Address...")
+  return codeChoice
+
 try:
   while True:
-    codeChoice = random.choice(codeList)
-    print("#Changing the IP Address...")
-    os.system("protonvpn-cli connect  --cc " + codeChoice)  #Concatinates Proton commands with country code(--cc) from the codelist
+    os.system("protonvpn-cli connect  --cc " + lottery())  #Concatinate Proton commands with country code(--cc) from the codelist
     os.system("protonvpn-cli status")
     sleep(random.randrange(600,900))
-except:
-  os.system("protonvpn-cli disconnect")
+except Exception as e:
   print("Error occured")
+  print(f"{str(e)}")
+finally:
+  os.system("protonvpn-cli disconnect")
