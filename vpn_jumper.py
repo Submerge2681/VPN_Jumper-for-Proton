0a54c24246fd967986760af72d75c44ff3b99b87
Submerge2681 <115612009+Submerge2681@users.noreply.github.com>
2024-01-03 19:33:12 +0530

    Update vpn_jumper.py
    Separated countries by tiers for varying levels of paranoia.
    Improved readability of code.
    Added timeout for connection so if the connection isn't made in 60s the code will auto reattempt.

--- vpn_jumper.py
+++ vpn_jumper.py
@@ -2,29 +2,40 @@
 import os
 from time import sleep
 import random
+import subprocess
 
-codeList = ["CH", "IS", "SE", "RO", "DE", "ES", "AT", "GR"] #Switzerland(CH), Iceland(IS), Sweden(SE), Romania(RO), Germany(DE), Spain(ES), Austria(AT), Greece(GR)
-#Germany, Sweden and spain are part of the 14 eyes.
+MIN_RANGE_IN_MIN = 15
+MAX_RANGE_IN_MIN = 20
 
-previous = ''
+t1Countries = ["CH", "IS", "SE", "RO", "DE", "ES", "AT", "GR"]
+t2Countries = ["PT", "IT", "IE", "NL", "BE", "LU", "DK", "NO", "FI", "CZ", "HU", "HR", "MT", "RS"]
+t3Countries = ["FR", "BR", "CL", "CO", "EE", "LV", "LT", "PL", "HK", "IN", "MD", "MA", "MK", "SG", "SK", "SL", "ZA", "VN"]
+countryList = t1Countries #+ t2Countries + t3Countries
 
-def lottery():
-  global previous
-  codeChoice = random.choice(codeList)
-  if codeChoice == previous:
-    while codeChoice == previous:
-      codeChoice = random.choice(codeList)
-  previous = codeChoice
-  print("\n#Changing the IP Address...")
-  return codeChoice
+previousCountry = ''
 
+# Random country
+def randomCountry ():
+  global previousCountry
+  newCountry = random.choice (countryList)
+  if newCountry == previousCountry:
+    while newCountry == previousCountry:
+      newCountry = random.choice (countryList)
+  previousCountry = newCountry
+  print ("\n# Changing the IP Address...")
+  return newCountry
+
+# Main
 try:
+  os.system ("protonvpn-cli killswitch --permanent")
   while True:
-    os.system("protonvpn-cli connect  --cc " + lottery())  #Concatinate Proton commands with country code(--cc) from the codelist
-    os.system("protonvpn-cli status")
-    sleep(random.randrange(600,900))
+    country = randomCountry()
+    subprocess.run(["protonvpn-cli", "connect", "--cc", country], timeout=60, check=True)
+    status = os.system ("protonvpn-cli status")
+    if status == 0:
+      sleep (random.randrange (MIN_RANGE_IN_MIN*60,MAX_RANGE_IN_MIN*60))
 except Exception as e:
-  print("Error occured")
-  print(f"{str(e)}")
+      print (f"Error occurred: {str (e)}\n Exiting...")
 finally:
-  os.system("protonvpn-cli disconnect")
+  os.system ("protonvpn-cli disconnect")
+  os.system ("protonvpn-cli killswitch --off")
